abfb3d2299f0c71cb8ae01f40600ad1c8abaab1e
herbayulet <sayaherbayu@gmail.com>
2024-05-29 16:10:09 +0700

    new schema

--- app/schema/index.ts
+++ app/schema/index.ts
@@ -1,9 +1,6 @@
 import { z } from "zod";
 
 export const schema = z.object({
-  fullname: z.string().nonempty({ message: "Fullname is required" }),
-  email: z
-    .string()
-    .email({ message: "Email is invalid" })
-    .nonempty({ message: "Email is required" }),
+  fullname: z.string().min(3, { message: "Fullname is required" }),
+  email: z.string().email({ message: "Email is invalid" }).endsWith(".com"),
 });
